3a22d060a2e5977d313817c161e00c468f6deb8a
Walter Bruch <118626933+Kampfer87-Hub@users.noreply.github.com>
2023-03-15 19:35:09 +0100

    Add files via upload

---
+++ StringExmpls.java
@@ -0,0 +1,11 @@
+public class StringExmpls {
+    public static void main(String[] args) {
+        String a = "Hello, Java! It's a Java Basic course";
+
+        System.out.println(a.length());
+        System.out.println(a.charAt(11));
+
+        System.out.println(a.substring(13,24));
+        System.out.println(a.substring(20));
+    }
+}
