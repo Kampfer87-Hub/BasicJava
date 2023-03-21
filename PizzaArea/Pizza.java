5961d7ef03f79056ee4cb96f2197d48de10d1b6f
Walter Bruch <118626933+Kampfer87-Hub@users.noreply.github.com>
2023-03-21 17:07:38 +0100

    Add files via upload

--- PizzaArea/Pizza.java
+++ PizzaArea/Pizza.java
@@ -3,9 +3,10 @@ package PizzaArea;
 public class Pizza {
     public static void main(String[] args) {
 
-        double d = 4;
+        double d = 4.2;
 
         PizzaCalculation pizzaArea = new PizzaCalculation(d);
+        System.out.println(pizzaArea.pizzaArea(d));
 
     }
 
