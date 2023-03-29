2683d189590491033c63580115a252b35a0eacd7
Walter Bruch <118626933+Kampfer87-Hub@users.noreply.github.com>
2023-03-29 17:30:52 +0200

    Add files via upload

---
+++ elevator/ElevatorDataCollection.java
@@ -0,0 +1,35 @@
+package elevator;
+
+import java.util.Scanner;
+
+public class ElevatorDataCollection {
+    public static void captureData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Specify the number of floors in the building: ");
+        int H = scanner.nextInt();
+
+        System.out.print("How many floors does the elevator go up : ");
+        int N = scanner.nextInt();
+
+        System.out.print("How many floors did the elevator go down: ");
+        int M = scanner.nextInt();
+
+
+        int sum = 0;
+        int count = 0;
+        while (sum < H) {
+            if (N > M) {
+                sum = sum + N - M;
+            } else {
+                System.out.println("Elevator can't move up");
+                break;
+            }
+            System.out.println(sum);
+            count++;
+        }
+
+        System.out.println("to go up to the " + H + "th floor, you have to do " + count + " climbs");
+
+        scanner.close();
+    }
+}
---
+++ elevator/ElevatorTest.java
@@ -0,0 +1,9 @@
+package elevator;
+
+public class ElevatorTest {
+    public static void main(String[] args) {
+    ElevatorDataCollection.captureData();
+
+
+    }
+}
